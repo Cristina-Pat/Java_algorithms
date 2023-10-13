3a7efc025c4798ae9062e88971c27750b2f4d950
Cristina-Pat <calota.cristina@gmail.com>
2023-10-13 16:48:00 +0100

    Add DiagonalDifference

---
+++ src/com/crisPat/DiagonalDifference.java
@@ -0,0 +1,23 @@
+package com.crisPat;
+
+import java.util.List;
+
+public class DiagonalDifference {
+    public static int solution(List<List<Integer>> arr) {
+        int sumLtR = 0;
+        int sumRtL = 0;
+
+        for (int i = 0; i < arr.size(); i++){
+            for(int j = 0; j< arr.get(i).size(); j++){
+                if(i == j){
+                    sumLtR+=arr.get(i).get(j);
+                }
+                if(j == arr.size() - 1 - i ){
+                    sumRtL+=arr.get(i).get(j);
+                }
+            }
+        }
+        return Math.abs(sumLtR - sumRtL) ;
+
+    }
+}
